51b3affba4c89c7be951c06e6f73bbcde26ca3c5
Tibor Benke <ihrwein@gmail.com>
2014-10-07 22:46:22 +0200

    setup.py: install jinyaml as a script

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='jinyaml',
       version='1.0',
       py_modules=['jinyaml'],
-      install_requires = ['PyYAML', 'Jinja2']
+      install_requires = ['PyYAML', 'Jinja2'],
+      scripts = ['jinyaml.py'],
+      packages = find_packages(),
+      entry_points = {
+        'console_scripts': [
+            'jinyaml = jinyaml:main'
+        ]
+      }
       )
